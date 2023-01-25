1d897df54ed7e0177839bc90bbc02dd5f00f770e
Ben Nassif <bennassif@bnassif.us>
2023-01-24 23:01:25 -0500

    Cleaning repo and code

--- setup.py
+++ setup.py
@@ -5,16 +5,22 @@ def read(filename):
     with open(filename, encoding='utf8', errors='ignore') as file:
         return file.read()
 
+def get_version() -> str:
+    """Get the version string from the module's __init__ file."""
+    with open("bootstrap/__init__.py") as init:
+        version = re.search(r'[\'"]\S\d*.\d*.\d*\S*[\'"]', init.read())
+        return version.group().strip('"').strip("'")
+
 setup(
     name="innodb_optimize",
-    version='0.1.4',
+    version=get_version(),
     description="Automated optimizer for MySQL InnoDB configurations",
     long_description=read("README.rst"),
     author="Ben Nassif",
     author_email="bennassif@gmail.com",
     maintainer="Ben Nassif",
     maintainer_email="bennassif@gmail.com",
-    url="https://www.linkedin.com/in/ben-nassif/",
+    url="https://github.com/Scraps23/innodb_optimize",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
